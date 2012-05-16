4bd6645fde638a39bb3793a6460be12513936356
burisu <burisu@oneiros.fr>
2012-05-16 23:18:22 +0200

    Moves Formize(d|r) (Rails 3.0) to Blab (Rails 3.2)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Formizer::Application.config.secret_token = '84aee473428f890fb30b7ca6e24e37f98c62ecad6d7cb0d2739230648df87776ba5152cc5c1159cc6f465181c8200f511170c4da5fd88f4a5f2673d11b770a12'
+Blab::Application.config.secret_token = '3b72a0e9e20c983800a256d6c9180fb99559bb4ec203025a29a3eaa412908587ede4b1e2a6db9795aad80121f5cef39b460f1ce705f0d7c4c7b551c049b3a128'
